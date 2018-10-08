0e6733a0deaff06844649a934bfc1742f9330d18
Rahul Gupta <gupta.rr@somaiya.edu>
2018-10-09 01:29:18 +0530

    2nd Commit

--- backend/app.js
+++ backend/app.js
@@ -1,92 +1,77 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const Post = require('./models/post');
 const mongoose = require("mongoose");
 
+const Post = require("./models/post");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://rahul:rahul0501@cluster0-fsgbw.mongodb.net/MEAN?retryWrites=true")
-.then(()=>{console.log("Connected to db");})
-.catch(e=>{console.log(e);});
+mongoose
+  .connect(
+    "mongodb+srv://rahul:rahul0501@cluster0-fsgbw.mongodb.net/MEAN?retryWrites=true"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   res.setHeader("Access-Control-Allow-Headers",
-      "Origin,X-Requested-With, Content-Type,Accept");
-   res.setHeader("Access-Control-Allow-Methods",
-      "GET,POST,PATCH,DELETE,OPTIONS");
-   next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
 });
 
 app.post("/api/posts", (req, res, next) => {
-   const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-   });
-  post.save();
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    });
+  });
+});
 
-   res.status(201).json({
-      message: 'Post added successfully',
-   })
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Update successful!" });
+  });
+});
 
-})
-app.use('/api/posts', (req, res, next) => {
-   // const posts = [
-   //    { id: 'hi123', title: 'First server-side post', content: 'This is comming from server' },
-   //    { id: 'hi122', title: 'Second server-side post', content: 'This is comming from server!' },
-   // ];
-   Post.find()
-   .then(documents=>{
-      console.log(documents);
-      res.status(200).json({
-         message: 'Posts fetched successfully',
-         posts: documents
-      });
-   });
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
 });
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const http = require('http');
-const app = require('./backend/app');
-const server = http.createServer(app);
-
-const port = process.env.PORT || 3000;
-app.set('port',port);
-server.listen(port,()=>{
-   console.log('Server is running on port:',port);
-});
-*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import {
   MatInputModule,
   MatCardModule,
@@ -14,7 +15,7 @@ import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
-import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { HttpClientModule } from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
